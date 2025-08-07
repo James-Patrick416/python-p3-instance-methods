8456c862e4c15584132029578393a8da9c640357
Your Name <you@example.com>
2025-08-07 00:26:22 -0700

    completed methods and called them on instances

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,7 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self): # this method prints woof when called on a dog instance
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
